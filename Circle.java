75dfb5e93cda7b5c3720ea376aa8c2a8f7dd64fb
FENG <22075501d@connect.polyu.hk>
2024-02-22 13:53:03 +0800

    circle

--- Circle.java
+++ Circle.java
@@ -17,17 +17,18 @@ class Circle extends Shape{
    
    // To compute the shape’s area
    public void computeArea(){
-       this.area = (float)  Math.PI * Math.pow(this.radius, 2);
+       this.area = (float) Math.PI * this.radius * this.radius ;
    };
    
    // To computer the shape’s perimeter
    public void computePerimeter(){
-       
+       this.perimeter = (float) Math.PI * 2 * this.radius;
    };
    
    // To display the area and perimeter of the shape
    public void displayShape(){
-       
+       System.out.println(" Area of circle = "+this.area);
+       System.out.println(" Perimeter of circle = "+this.perimeter);
    };
    
 }
